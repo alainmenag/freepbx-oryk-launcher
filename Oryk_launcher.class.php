c3334b5a6ddb9caa5360fe4380c7ba272aff4802
Sebastian Alain <alainmenag@gmail.com>
2025-08-03 14:53:56 -0400

    Launcher items are based on access

--- Oryk_launcher.class.php
+++ Oryk_launcher.class.php
@@ -25,19 +25,78 @@ class Oryk_launcher extends FreePBX_Helpers implements \BMO
 
 		switch ($page) {
 			case 'index':
-
 				return load_view(__DIR__ . '/views/launcher.php', [
-					'users' => ['count' => $this->getCount('userman_users')],
-					'extensions' => ['count' => $this->getCount('users')],
-					'trunks' => ['count' => $this->getCount('trunks')],
-					'inbound_routes' => ['count' => $this->getCount('inbound_routes')],
-					'outbound_routes' => ['count' => $this->getCount('outbound_routes')],
-					'voicemail' => ['count' => $this->getCount('voicemail')],
-					'call_logs' => ['count' => $this->getCount('cdr')]
+					'items' => $this->getLaunchable(),
 				]);
 			default:
 				break;
 		}
+	}
+
+	public function getLaunchable()
+	{
+		$launchable = [];
+
+		if (!isset($_SESSION['AMP_user'])) {
+			return $launchable;
+		}
+
+		if ($_SESSION['AMP_user']->checkSection('userman')) array_push($launchable, [
+			'label' => 'Users',
+			'count' => $this->getCount('userman_users'),
+			'icon' => 'fa-users',
+			'link' => '/admin/config.php?display=userman'
+		]);
+
+		if ($_SESSION['AMP_user']->checkSection('extensions')) array_push($launchable, [
+			'label' => 'Extensions',
+			'count' => $this->getCount('users'),
+			'icon' => 'fa-phone',
+			'link' => '/admin/config.php?display=extensions'
+		]);
+
+		if ($_SESSION['AMP_user']->checkSection('trunks')) array_push($launchable, [
+			'label' => 'Trunks',
+			'count' => $this->getCount('trunks'),
+			'icon' => 'fa-plug',
+			'link' => '/admin/config.php?display=trunks'
+		]);
+
+		if ($_SESSION['AMP_user']->checkSection('inbound_routes')) array_push($launchable, [
+			'label' => 'Inbound Routes',
+			'count' => $this->getCount('incoming'),
+			'icon' => 'fa-road',
+			'link' => '/admin/config.php?display=did'
+		]);
+
+		if ($_SESSION['AMP_user']->checkSection('outbound_routes')) array_push($launchable, [
+			'label' => 'Outbound Routes',
+			'count' => $this->getCount('outbound_routes'),
+			'icon' => 'fa-phone-square',
+			'link' => '/admin/config.php?display=routing'
+		]);
+
+		// if ($_SESSION['AMP_user']->checkSection('voicemail')) array_push($launchable, [
+		// 	'label' => 'Voicemail',
+		// 	'count' => $this->getCount('voicemail'),
+		// 	'icon' => 'fa-microphone',
+		// 	'link' => '/admin/config.php?display=voicemail'
+		// ]);
+
+		if ($_SESSION['AMP_user']->checkSection('cdr')) array_push($launchable, [
+			'label' => 'CDRs',
+			'count' => $this->getCount('cdr'),
+			'icon' => 'fa-history',
+			'link' => '/admin/config.php?display=cdr'
+		]);
+
+		array_push($launchable, [
+			'label' => 'GUI',
+			'icon' => 'fa-paint-brush',
+			'link' => '/admin/config.php?display=oryk_gui'
+		]);
+		
+		return $launchable;
 	}
 
 	public function getCount($table = null)
