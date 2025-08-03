1e3e6187a21dff6926cfbbecc4ad237c03ab299f
Sebastian Alain <alainmenag@gmail.com>
2025-08-03 04:52:53 -0400

    Something isn't loading

--- functions.inc.php
+++ functions.inc.php
@@ -7,4 +7,11 @@
 // }
 
 
+function oryk_launcher_hook_core() {
+    global $admin;
+    $admin->addJS('/admin/modules/oryk_launcher/js/launcher.js'); // if needed
+    $admin->addCSS('/admin/modules/oryk_launcher/css/launcher.css');
+}
+
+
 ?>
